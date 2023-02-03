98473e79570b6c21c01a01726e30f5f483443666
jojobaa <joycarvalho59@gmail.com>
2023-02-03 10:44:04 -0300

    initial configs

---
+++ src/controllers/hotels-controllers.ts
@@ -0,0 +1,9 @@
+import hotelService from "@/services/hotels-service";
+import { AuthenticatedRequest } from "@/middlewares";
+import { Response } from "express";
+import httpStatus from "http-status";
+
+export async function getHotels(req: AuthenticatedRequest, res: Response) {
+  const hotels = await hotelService.getHotels();
+  return res.status(httpStatus.OK).send(hotels);
+}
---
+++ src/services/hotels-service/index.ts
@@ -0,0 +1,12 @@
+import hotelRepository from "@/repositories/hotel-repository";
+
+async function getHotels() {
+    const hotels = await hotelRepository.findHotels();
+    return hotels;
+}
+
+const hotelService = {
+    getHotels
+};
+
+export default hotelService;
